4c9600e163944efa54485c3072d04484ad9133db
Razvan Serbanescu <razvan@pionix.ro>
2019-09-03 12:26:39 +0300

    fixed cordova.exec call

--- www/referrer.js
+++ www/referrer.js
@@ -1,5 +1,3 @@
-var exec = require('cordova/exec');
-
 function getReferrer(success, error) {
     if (!success) {
         return new Promise(function (resolve, reject) {
@@ -7,7 +5,7 @@ function getReferrer(success, error) {
         });
     }
 
-    exec(function (result) {
+    cordova.exec(function (result) {
         if (result) {
             success(result);
         } else {
